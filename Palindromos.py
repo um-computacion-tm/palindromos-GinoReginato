dfe0d0478869e8c3e54235ddba374bf628baaafd
Gino <gp.reginato@alumno.um.edu.ar>
2024-04-06 16:18:24 -0300

    Update palindromos

--- Palindromos.py
+++ Palindromos.py
@@ -1,33 +1,34 @@
-#TEST PALINDROME
+#PALINDROME TEST
 
 import unittest
 
 def is_palindrome(mystring):
     for indice in range(len(mystring)):
-        print(mystring (indice) + " -> " + mystring[-(indice +1)])
-        if mystring[indice] != mystring[-(indice +1)]:
+        if mystring[indice] != mystring[-(indice + 1)]:
             print("no es")
             return False
-        return True 
+    print("es")
+    return True
+    cleaned_string = mystring.replace(" ", "").lower()
+    
+    return cleaned_string == cleaned_string[::-1]
 
 class TestPalindrome(unittest.TestCase):
     def test_a(self):
         resultado = is_palindrome("AcA")
         self.assertEqual(resultado, True)
 
-    def test_a(self):
-        resultado = is_palindrome("ABCAC")
-        self.assertEqual(resultado, True)
+    def test_b(self):
+        resultado = is_palindrome("ABCA")
+        self.assertEqual(resultado, False)
 
-
-class TestPalindrome(unittest.TestCase):
-    def test_a(self):
+    def test_c(self):
         resultado = is_palindrome("neuquen")
         self.assertEqual(resultado, True)
 
-    def test_a(self):
-        resultado = is_palindrome("neuqu en")
+    def test_d(self):
+        resultado = is_palindrome("ne uqu enL")
         self.assertEqual(resultado, True)
 
-
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
